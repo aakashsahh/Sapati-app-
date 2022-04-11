9349b8842153bcb1b951c2d39c8ff8653dca3012
aakashsahh <aakash@academiacollege.edu.np>
2022-04-11 11:32:31 +0545

    sign up button added

--- lib/Button.dart
+++ lib/Button.dart
@@ -7,20 +7,36 @@ class Button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      margin: EdgeInsets.symmetric(horizontal: 50),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 23, 8, 228),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: const Center(
-        child: Text(
-          "Login",
-          style: TextStyle(
-              color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          margin: EdgeInsets.symmetric(horizontal: 50),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 23, 8, 228),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Center(
+            child: Text(
+              "Login",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
         ),
-      ),
+        SizedBox(
+          height: 40,
+        ),
+        Text(
+          "Sign Up",
+          style: TextStyle(color: Colors.grey),
+        ),
+        SizedBox(
+          height: 40,
+        ),
+      ],
     );
   }
 }
